6be239add4f2ab96ddafd15fa45c1547d5df3fb4
Andre Roque <mateusandre1@yahoo.com>
2019-10-06 04:13:59 -0400

    intro added for customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
-
+var Table = require("cli-table2")
+var chalk = require("chalk");
+var log = console.log;
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -13,12 +15,20 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
   // console.log(connection);
   if (err) throw err;
-
+  inventory();
 });
 
 
 var inventory = function () {
 
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    log(chalk.green.bold(`\n    ----------------------------------------------\n
+    ---  WELCOME TO BAMAZON!! A One Stop Shop! ---\n
+    ---         Find Our Prodcuts Below        ---\n
+    ----------------------------------------------\n`));
 
+    connection.end();
+  });
 
 }
