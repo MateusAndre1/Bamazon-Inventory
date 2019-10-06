5a4ed039430f3717b9643daa0e4bb6d9f931f73c
Andre Roque <mateusandre1@yahoo.com>
2019-10-06 03:47:46 -0400

    h

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,17 +3,22 @@ var mysql = require("mysql");
 
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "rootroot",
-    database: "bamazon_db"
-  });
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "rootroot",
+  database: "bamazon_db"
+});
 
-  connection.connect(function(err) {
-    // console.log(connection);
-      if (err) throw err;
-      
-    });
-    
-    
+connection.connect(function (err) {
+  // console.log(connection);
+  if (err) throw err;
+
+});
+
+
+var inventory = function () {
+
+
+
+}
